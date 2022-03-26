220e084d629d697fc3db1caaa420ac17af5aae14
SAITO Kazuya <saito@smartalgorithm.co.jp>
2022-03-26 13:58:07 +0900

    Generate mix project

---
+++ lib/ex_demo.ex
@@ -0,0 +1,18 @@
+defmodule ExDemo do
+  @moduledoc """
+  Documentation for `ExDemo`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExDemo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
